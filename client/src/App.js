0b7c746ad24ed1a3ae8ca1d6187beff5ad040312
Pintu Kumar <kumarpintu1713122@gmail.com>
2022-03-28 08:30:39 +0530

    router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,8 @@
-import React from 'react'
 import Landing from './pages/Landing'
 
 function App() {
   return (
     <div>
-      <h1>jobify</h1>
       <Landing />
     </div>
   )
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,11 +1,30 @@
-import logo from '../assets/images/logo.svg'
-import main from '../assets/images/main.svg'
+import main from '../assets/images/main-alternative.svg'
+import Wrapper from '../assets/wrappers/LandingPage'
+import { Logo } from '../components'
 
 const Landing = () => {
     return (
-        <div>
-
-        </div>
+        <Wrapper>
+            <nav>
+                <Logo />
+            </nav>
+            <div className="container page">
+                <div className="info">
+                    <h1>
+                        Job <span>Tracking</span> App
+                    </h1>
+                    <p>
+                        I'm baby blue bottle whatever echo park cred street
+                        art meditation plaid you probably haven't
+                        heard of them iceland umami.
+                        Kogi wayfarers yr tofu banh mi four dollar toast
+                        tumeric chia vice air plant.
+                    </p>
+                    <button className="btn btn-hero">Login/Register</button>
+                </div>
+                <img src={main} alt="job hunt" className="img main-img" />
+            </div>
+        </Wrapper>
     )
 }
 
